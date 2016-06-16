78b62431398cf1d4eb04d15803cef1694ed56a64
Joseph Lozano <lozanojoseph@gmail.com>
2016-06-15 20:09:10 -0700

    mget row

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 usage:
 ```Elixir
 {:ok, pid} = HBase.Client.start_link("0.0.0.0", 9090) # host and port
-HBase.Client.get(pid, tablename, row)
+{"row", results_map} = HBase.Client.get(pid, "tablename", "row")
+[{"row1", results_map1}, {"row2", results_map2}] = HBase.Client.mget(pid, tablename, ["row1", "row2"])
 ```
--- lib/hbase/hbase_client.ex
+++ lib/hbase/hbase_client.ex
@@ -1,6 +1,7 @@
 defmodule HBase.Client do
   use GenServer
 
+  # Initialization
   def start_link(host, port) do
     args = {String.to_char_list(host), port}
     GenServer.start_link(__MODULE__, args, [])
@@ -11,25 +12,45 @@ defmodule HBase.Client do
     {:ok, client}
   end
 
+  # Get
   def get(pid, table, row) do
     GenServer.call(pid, {:get, table, row})
   end
 
+
+  # mget
+  def mget(pid, table, rows) when is_list(rows) do
+    GenServer.call(pid, {:mget, table, rows})
+  end
+
+  # callbacks
   def handle_call({:get, table, row}, _from, client) do
-    {client, response} = :thrift_client.call(client, :getRow, [table, row, :dict.new()])
-    {:ok, [{:TRowResult, ^row, dict, :undefined}]} = response
-    result =
+    {client, {:ok, response}} = :thrift_client.call(client, :getRow, [table, row, :dict.new()])
+    [result] = parse_get_response(response)
+    {:reply, result, client}
+  end
+
+  def handle_call({:mget, table, rows}, _from, client) do
+    {client, {:ok, response}} = :thrift_client.call(client, :getRows, [table, rows, :dict.new()])
+    result = parse_get_response(response)
+    {:reply, result, client}
+  end
+
+  # Private
+  defp parse_get_response(response) do
+    response
+    |> Enum.map( fn({:TRowResult, row, dict, :undefined}) ->
       dict
       |> :dict.to_list
-      |> Enum.map(fn(el) ->
-        case el do
-          {column, {:TCell, <<value::64>>, _timestamp}} ->
-            {column, value} 
-          {column, {:TCell, value, _timestamp}} ->
-            {column, value}
-        end
+      |> Enum.map(fn
+        {column, {:TCell, <<value::64>>, _timestamp}} ->
+          {column, value}
+        {column, {:TCell, value, _timestamp}} ->
+          {column, value}
       end)
       |> Map.new
-    {:reply, result, client}
+      |> Tuple.duplicate(1)
+      |> Tuple.insert_at(0, row)
+    end)
   end
 end
